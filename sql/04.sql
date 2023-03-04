6ca04d1e32a5a9399b9b67b653b10d1e2467b8f1
Ninja <nmukneam@students.pitzer.edu>
2023-03-03 17:28:16 -0800

    fixed order by problem 4

--- sql/04.sql
+++ sql/04.sql
@@ -10,4 +10,4 @@
 --
 --
 
-select first_name, last_name from ((select distinct actor_id from (select film_id from film join film_category using (film_id) where category_id = 3)child join film_actor using (film_id)) except (select distinct actor_id from (select film_id from film join film_category using (film_id) where category_id = 11)horror join film_actor using (film_id)))fin join actor using (actor_id) order by last_name;
+select first_name, last_name from ((select distinct actor_id from (select film_id from film join film_category using (film_id) where category_id = 3)child join film_actor using (film_id)) except (select distinct actor_id from (select film_id from film join film_category using (film_id) where category_id = 11)horror join film_actor using (film_id)))fin join actor using (actor_id) order by last_name, first_name;
